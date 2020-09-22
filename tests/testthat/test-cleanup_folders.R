baaf1b1aff729398b46f002f82d9208326282b93
Brian High <high@uw.edu>
2020-09-22 15:16:57 -0700

    Added tests to cleanup_folders.

--- tests/testthat/test-cleanup_folders.R
+++ tests/testthat/test-cleanup_folders.R
@@ -1,5 +1,24 @@
-test_that("cleanup_folders works", {
+test_that("cleanup_folders works with defaults", {
   folders <- get_folders()
+  expect_equal(file.exists(here::here('folders.yml')), TRUE)
+  result <- create_folders(folders)
+  cleanup_result <- cleanup_folders(folders)
+  names(cleanup_result) <- basename(names(cleanup_result))
+  cleanup_result_default <- c(
+    code = 0,
+    data = 0,
+    doc = 0,
+    figures = 0,
+    results = 0
+  )
+  expect_equal(cleanup_result, cleanup_result_default)
+  expect_equal(file.exists(here::here('folders.yml')), TRUE)
+  expect_equal(unlink(here::here('folders.yml')), 0)
+})
+
+test_that("cleanup_folders works with configuration file removal", {
+  folders <- get_folders()
+  expect_equal(file.exists(here::here('folders.yml')), TRUE)
   result <- create_folders(folders)
   cleanup_result <- cleanup_folders(folders, keep_conf = FALSE)
   names(cleanup_result) <- basename(names(cleanup_result))
@@ -12,5 +31,30 @@ test_that("cleanup_folders works", {
     conf_file = 0
   )
   expect_equal(cleanup_result, cleanup_result_default)
+  expect_equal(file.exists(here::here('folders.yml')), FALSE)
 })
 
+test_that("cleanup_folders works with non-empty folders", {
+  folders <- get_folders()
+  result <- create_folders(folders)
+  file_path <- here::here(folders$data, 'data.csv')
+  write.csv(data.frame(x = 1:3), file_path)
+  cleanup_result <- cleanup_folders(folders)
+  names(cleanup_result) <- basename(names(cleanup_result))
+  cleanup_result_default <- list(
+    code = 0,
+    data = NULL,
+    doc = 0,
+    figures = 0,
+    results = 0
+  )
+  expect_equal(cleanup_result, cleanup_result_default)
+  expect_equal(file.exists(here::here('folders.yml')), TRUE)
+  expect_equal(file.exists(file_path), TRUE)
+  unlink(here::here('folders.yml'))
+  unlink(file_path)
+  unlink(here::here(folders$data), recursive = TRUE)
+  expect_equal(file.exists(here::here('folders.yml')), FALSE)
+  expect_equal(dir.exists(here::here(folders$data)), FALSE)
+  expect_equal(file.exists(file_path), FALSE)
+})
