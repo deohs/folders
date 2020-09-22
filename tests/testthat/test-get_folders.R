660267ec711e627fe23d648b4082e6ad527b80a8
Brian High <high@uw.edu>
2020-09-22 15:43:17 -0700

    Updated tests.

--- tests/testthat/test-get_folders.R
+++ tests/testthat/test-get_folders.R
@@ -1,5 +1,6 @@
 test_that("get_folders works", {
   folders <- get_folders()
+  expect_equal(file.exists(here::here('folders.yml')), TRUE)
   folders_default <- list(
     code = 'code',
     data = 'data',
@@ -7,6 +8,6 @@ test_that("get_folders works", {
     figures = 'figures',
     results = 'results'
   )
-  cleanup_result <- cleanup_folders(folders, keep_conf = FALSE)
+  unlink(here::here('folders.yml'))
   expect_equal(folders, folders_default)
 })
